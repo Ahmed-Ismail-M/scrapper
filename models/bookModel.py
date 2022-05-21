e467c00e06f253bf0867f2184c560762dbcd93b0
ahmed <ahmedismailm85@gmail.com>
2022-05-21 12:38:57 +0200

    delete datastore

--- models/bookModel.py
+++ models/bookModel.py
@@ -3,5 +3,6 @@ from dataclasses import dataclass
 
 @dataclass
 class Book:
+    id: int
     title: str
     auther: str
--- server.py
+++ server.py
@@ -2,11 +2,16 @@ from flask import Flask
 from flask.globals import request
 
 from datastore import DataStore
+from models.bookModel import Book
+from services.ExcelService import ExcelService
 app = Flask(__name__)
-
+excel_service = ExcelService(path='test.xlsl')
 @app.route('/', methods=['GET'])
 def home():
     if request.method == 'GET':
-        DataStore().add()
+        
+    if request.method == "POST":
+        book = Book(id= int(request.form['id']), title= request.form['title'], auther= request.form['title'])
+      ``
         return('hi')
 app.run(host='localhost', port=8000)
