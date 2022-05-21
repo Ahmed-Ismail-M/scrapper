352dffabe7fd81dec27dcdf2d22cfa2edee3374a
ahmed <ahmedismailm85@gmail.com>
2022-05-21 12:23:50 +0200

    feat: create wb

--- index.py
+++ index.py
@@ -1,25 +1,32 @@
 import openpyxl
 import requests
 from bs4 import BeautifulSoup
+
+from services.ExcelService import ExcelService
+
 URL = "https://ar.wikipedia.org/wiki/%D9%82%D8%A7%D8%A6%D9%85%D8%A9_%D8%A3%D9%81%D8%B6%D9%84_%D9%85%D8%A6%D8%A9_%D8%B1%D9%88%D8%A7%D9%8A%D8%A9_%D8%B9%D8%B1%D8%A8%D9%8A%D8%A9"
 PATH = 'test.xlsx'
 def write_to_excel(url:str, path:str):
     res = requests.get(url=url).text
     soup = BeautifulSoup(res, "html.parser")
-    wb = openpyxl.Workbook()  # open workboopk
-    ws = wb.active  # open sheet
+    excel_service = ExcelService()
+    excel_service.creat_wb()
     result = soup.find("table", class_="wikitable").find_all("tr")[1::1]  # type: ignore
     for row, items in enumerate(result):
         data = items.find_all(["th", "td"])
-        links = items.find_all("a")
+        links = items.find_all("a", href=True)
         for col in range(len(data)):
             try:
+                
+                value = str(data[col].text)
                 if col <= len(links):
-                    ws.cell(row + 1, col + 1, str(data[col].text)).hyperlink = "https://ar.wikipedia.org/wiki/" + links[col - 1].text  # type: ignore
+                    hyperlink = "https://ar.wikipedia.org/" + links[col-1]['href']  # type: ignore
+                    excel_service.write_to_excel_with_hyperlink(row=row+1, col=col+1, data=value, hyperlink=hyperlink )
                 else:
-                    ws.cell(row + 1, col + 1, str(data[col].text))
+                    excel_service.write_to_excel(row=row+1, col=col+1, data=value)
             except IndexError:
                 pass
-    wb.save(path)
+
+    excel_service.save(path=path)
 write_to_excel(url=URL, path=PATH)
 
--- services/ExcelService.py
+++ services/ExcelService.py
@@ -1,11 +1,19 @@
 
 import openpyxl
 
-class ExcelServer:
+class ExcelService:
     def __init__(self) -> None:
+        self.wb = None
+        self.ws = None
+    def creat_wb(self):
         self.wb = openpyxl.Workbook()  # open workboopk
         self.ws = self.wb.active  # open sheet
     def write_to_excel_with_hyperlink(self, row:int, col:int, data:str, hyperlink:str):
-        self.ws.cell(row + 1, col + 1, str(data)).hyperlink = hyperlink # type: ignore
+        if self.ws:
+            self.ws.cell(row + 1, col + 1, data).hyperlink = hyperlink # type: ignore
     def write_to_excel(self, row:int, col:int, data:str):
-        self.ws.cell(row + 1, col + 1, str(data))
+        if self.ws:
+            self.ws.cell(row + 1, col + 1, data)
+    def save(self, path: str):
+        if self.wb:
+            self.wb.save(path)
