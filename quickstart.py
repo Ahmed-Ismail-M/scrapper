d0f5bffd7e0b98560af42feab6b4686afbf18b55
ahmed <ahmedismailm85@gmail.com>
2022-05-21 21:51:29 +0200

    add speadsheets api and writable

--- quickstart.py
+++ quickstart.py
@@ -9,7 +9,7 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from google.oauth2 import service_account
 # If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
 # The ID and range of a sample spreadsheet.
 SAMPLE_SPREADSHEET_ID = '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms'
@@ -42,21 +42,29 @@ def main():
 
     try:
         service = build('sheets', 'v4', credentials=creds)
-
+        spreadsheet = {
+            'properties': {
+                'title': 'testSS'
+            }
+        }
+        spreadsheet = service.spreadsheets().create(body=spreadsheet,
+                                                    fields='spreadsheetId').execute()
+        ss_id = spreadsheet.get('spreadsheetId')
+        
         # Call the Sheets API
-        sheet = service.spreadsheets()
-        result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                    range=SAMPLE_RANGE_NAME).execute()
-        values = result.get('values', [])
+        # sheet = service.spreadsheets()
+        # result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+        #                             range=SAMPLE_RANGE_NAME).execute()
+        # values = result.get('values', [])
 
-        if not values:
-            print('No data found.')
-            return
+        # if not values:
+        #     print('No data found.')
+        #     return
 
-        print('Name, Major:')
-        for row in values:
-            # Print columns A and E, which correspond to indices 0 and 4.
-            print('%s, %s' % (row[0], row[4]))
+        # print('Name, Major:')
+        # for row in values:
+        #     # Print columns A and E, which correspond to indices 0 and 4.
+        #     print('%s, %s' % (row[0], row[4]))
     except HttpError as err:
         print(err)
 
