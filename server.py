176d7cf90439bdd9ae3672fb10b7c9079e105b47
ahmed <ahmedismailm85@gmail.com>
2022-05-21 15:57:25 +0200

    edit the excel sheet

--- server.py
+++ server.py
@@ -12,26 +12,24 @@ def home():
     if request.method == "GET":
         return data_store.get_all_books()
     if request.method == "POST" or request.method == "PUT":
-        print('putting')
         if request.method == "POST":
             id = data_store.get_last_id()
         else:
-            id = request.values.get("book_id")
+            if request.values.get("book_id"):
+                id = int(request.values.get("book_id"))
         title = request.values.get("title")
         author = request.values.get("author")
         country = request.values.get("country")
-        if id and title and author and country:
-            book = Book(
-                id=data_store.get_last_id(), title=title, author=author, country=country
-            )
-            if book:
-                try:
-                    data_store.add(book=book)
-                    return "added"
-                except PermissionError:
-                    return "Excel File is locked"
-        else:
-            return "Missing inputs"
+        book = Book(
+            id=id, title=title, author=author, country=country
+        )
+        if book:
+            try:
+                data_store.add(book=book)
+                return "added"
+            except PermissionError:
+                return "Excel File is locked"
+       
 
     if request.method == "DELETE":
         book_id = request.values.get("book_id")
