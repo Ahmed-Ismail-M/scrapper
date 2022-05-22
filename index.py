73e68245b9796d9f6de02e533f64baaf3be97661
ahmed <ahmedismailm85@gmail.com>
2022-05-22 14:03:06 +0200

    refactor: add variables at top

--- index.py
+++ index.py
@@ -3,12 +3,13 @@ from bs4 import BeautifulSoup
 from services.ExcelService import ExcelService
 from services.GsheetService import GSheetService
 from services.SheetInterface import SheetService
-def main():
-    write_to_excel(url=URL)
-    write_to_gsheet(url=URL)
 URL = "https://ar.wikipedia.org/wiki/%D9%82%D8%A7%D8%A6%D9%85%D8%A9_%D8%A3%D9%81%D8%B6%D9%84_%D9%85%D8%A6%D8%A9_%D8%B1%D9%88%D8%A7%D9%8A%D8%A9_%D8%B9%D8%B1%D8%A8%D9%8A%D8%A9"
 EXCEL_PATH = 'test.xlsx'
 GSHEET_PATH = 'books'
+def main():
+    write_to_excel(url=URL)
+    write_to_gsheet(url=URL)
+
 def write_to_excel(url:str):
     sheet_service=ExcelService(path=EXCEL_PATH)
     res = requests.get(url=url).text
