63feb6e4dcf6792f7a2ac57153fe82212f27f26f
ahmed <ahmedismailm85@gmail.com>
2022-05-23 07:42:50 +0200

    feat: from html to pdf

--- services/QRService.py
+++ services/QRService.py
@@ -1,26 +1,30 @@
-from reportlab.pdfgen import canvas
 import qrcode
 import qrcode.image.svg as qimg
-import tempfile
 from reportlab.graphics import renderPDF
 from svglib.svglib import svg2rlg
+from reportlab.platypus import SimpleDocTemplate
+from reportlab import platypus
+from  reportlab.lib.styles import ParagraphStyle as PS
+import os
 
 PATH = 'qr.svg'
-def create(url:str):
-    img = qrcode.make(url,image_factory=qimg.SvgImage)
+# PATH = 'accounting1.png'
+def create_svg(url:str):
+    img = qrcode.make(url,image_factory=qimg.SvgPathImage)
     with open(PATH,'wb') as qr:
         img.save(qr)
 
-# def create_img():
-#     img = qrcode.make('www.im-software.net',image_factory=qimg.SvgImage)
-#     svg_file = tempfile.NamedTemporaryFile()
-#     img.save(svg_file)
-#     svg_file.flush()
-#     img_rl = svg2rlg(svg_file.name)
-#     svg_file.close()
-#     return img_rl
-def create_pdf(path):
-    create('www.im-software.net')
-    renderPDF.drawToFile(svg2rlg(PATH), PATH)
-
-create_pdf('test.pdf')
+def create_pdf(path, url):
+    create_svg(url)
+    img = svg2rlg(PATH)
+    renderPDF.drawToFile(img, path)
+def create_from_string(path):
+    create_svg('https://im-software.net/')
+    items = []
+    text = f"""<a href="/wiki/%D9%84%D8%B9%D8%A8%D8%A9_%D8%A7%D9%84%D9%86%D8%B3%D9%8A%D8%A7%D9%86">
+    <img src="{PATH}" ></img></a>"""
+    items.append(platypus.Paragraph(text, PS('body') ))
+    doc = SimpleDocTemplate(path)
+    doc.multiBuild(items)
+# create_pdf('test.pdf', 'https://im-software.net/')
+create_from_string('test2.pdf')
